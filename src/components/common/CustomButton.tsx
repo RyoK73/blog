2b2e9db0b5912ab6324ca20eb39b5208fde981bd
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-10 15:25:22 +0900

    chore:外部からclassNameを受け取るよう変更

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,13 +1,19 @@
+import { cn } from "@/lib/utils";
+
 type ButtonProps = {
     children: React.ReactNode;
     onClick: () => void;
+    className?: string;
 };
-export const CustomButton = ({ children, onClick }: ButtonProps) => {
+export const CustomButton = ({ children, onClick, className }: ButtonProps) => {
     return (
         <button
             type="button"
             onClick={onClick}
-            className="text-3xl p-2 transition hover:scale-120 active:scale-95 duration-500"
+            className={cn(
+                "text-3xl p-2 transition hover:scale-120 active:scale-95 duration-500",
+                className,
+            )}
         >
             {children}
         </button>
