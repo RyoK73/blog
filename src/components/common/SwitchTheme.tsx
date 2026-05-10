55b34fc2ac21ac9fe2c091478cccd473d1246811
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-10 15:23:50 +0900

    chore:ファイル名をSwitchThemeButtonからSwitchThemeへ変更
    - コンポーネント名と不一致だったため
    - 外部からclassNameを受け取るよう変更

---
+++ src/components/common/SwitchTheme.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useTheme } from "next-themes";
+import { LuSun } from "react-icons/lu";
+import { LuMoonStar } from "react-icons/lu";
+import { CustomButton } from "./CustomButton";
+
+export const SwitchTheme = ({ className }: { className?: string }) => {
+    const { setTheme, theme } = useTheme();
+    return (
+        <CustomButton
+            onClick={() => {
+                theme == "dark" ? setTheme("light") : setTheme("dark");
+            }}
+            className={className}
+        >
+            {theme == "dark" ? <LuSun /> : <LuMoonStar />}
+        </CustomButton>
+    );
+};
