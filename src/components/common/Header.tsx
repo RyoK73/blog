fe17d94c998ad266077f2afde5054e30d6ed8d5a
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-14 01:58:53 +0900

    feat:ブログアイコン・ファビコンを追加
    - ヘッダーにアイコンを追加
    - ファビコンを追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,38 +1,50 @@
+import Image from "next/image";
+import localImage from "../../../public/topImage.png";
 import { SwitchTheme } from "@/components/common/SwitchTheme";
-import { CustomTabs, type TabProp, NonEmptyArray } from "@/components/common/CustomTabs";
+import {
+  CustomTabs,
+  type TabProp,
+  NonEmptyArray,
+} from "@/components/common/CustomTabs";
 const TabProps: NonEmptyArray<TabProp> = [
-    {
-        name: "01 . HOME",
-        link: "/",
-    },
-    {
-        name: "02 . WORK",
-        link: "/blog/tag/works",
-    },
-    {
-        name: "03 . ABOUT",
-        link: "/about",
-    },
+  {
+    name: "01 . HOME",
+    link: "/",
+  },
+  {
+    name: "02 . WORK",
+    link: "/blog/tag/works",
+  },
+  {
+    name: "03 . ABOUT",
+    link: "/about",
+  },
 ];
 
 export const Header = () => {
-    const tabSize = "w-35 h-auto";
-    return (
-        <header className="col-span-2 flex items-center border border-border m-8 h-25 divide-x divide-border bg-background/20 text-input">
-            <div className="flex-1 flex p-4 h-full">
-                <p className=" border border-border flex justify-center items-center mr-4">. . /</p>
-                <div className="text-border">
-                    <p>Personal Log</p>
-                    <div className="flex items-end gap-2">
-                        <h1 className="text-foreground text-3xl font-bold">DEV活</h1>
-                        <p>RyoK73@omarchy</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex h-full justify-end item-center ">
-                <CustomTabs tabs={TabProps} className={`${tabSize}`} />
-            </div>
-            <SwitchTheme className={`flex justify-center items-center ${tabSize}`} />
-        </header>
-    );
+  const tabSize = "w-35 h-auto";
+  return (
+    <header className="col-span-2 flex items-center border border-border m-8 h-25 divide-x divide-border bg-background/20 text-input">
+      <div className="flex-1 flex p-4 h-full">
+        <Image
+          src={localImage}
+          alt="トップ画像"
+          height={64}
+          width={64}
+          className="mr-4 rounded-full object-cover"
+        />
+        <div className="text-border">
+          <p>Personal Log</p>
+          <div className="flex items-end gap-2">
+            <h1 className="text-foreground text-3xl font-bold">DEV活</h1>
+            <p>RyoK73@omarchy</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex h-full justify-end item-center ">
+        <CustomTabs tabs={TabProps} className={`${tabSize}`} />
+      </div>
+      <SwitchTheme className={`flex justify-center items-center ${tabSize}`} />
+    </header>
+  );
 };
