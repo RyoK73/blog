d435104487b6e33dea6529622ce569a4ff005b06
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-18 17:38:50 +0900

    feat:タグ規定を追加
    - user-tag.jsonに存在しないタグは記事に書いてあろうが表示できないように
    - github-actionsで別途cdを設定予定

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import path from "path";
 import { Badge } from "../ui/badge";
 import { cn } from "@/lib/utils";
+import tagJson from "@/user-tag.json";
 
 // tagsを受け取って、Linkをバッジ表示する。各リンク先は、./@/app/blog/tag/[tag]へ
 type TagProps = {
@@ -12,19 +13,21 @@ export const TagList = ({ tags, className }: TagProps) => {
     return (
         <div className="flex flex-wrap gap-1.5 mt-1">
             {tags.map((tag) => {
-                return (
-                    <Link key={tag} href={path.join("/blog/tag", tag)}>
-                        <Badge
-                            variant={"outline"}
-                            className={cn(
-                                "cursor-pointer rounded-none text-card-foreground border border-border p-2",
-                                className,
-                            )}
-                        >
-                            {tag}
-                        </Badge>
-                    </Link>
-                );
+                if (Object.keys(tagJson).includes(tag)) {
+                    return (
+                        <Link key={tag} href={path.join("/blog/tag", tag)}>
+                            <Badge
+                                variant={"outline"}
+                                className={cn(
+                                    "cursor-pointer rounded-none text-card-foreground border border-border p-2",
+                                    className,
+                                )}
+                            >
+                                {tag}
+                            </Badge>
+                        </Link>
+                    );
+                }
             })}
         </div>
     );
