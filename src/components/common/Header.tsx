a8135146c88b9d45b1a3a885148cfe997fec558f
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-20 10:19:08 +0900

    feat:Headerの構成を変更
    - header>CustomCard(>Link:トップ画像等)+div(>div+switchTheme:タブ等)の構成に変更

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,6 +7,7 @@ import {
     type TabProp,
     NonEmptyArray,
 } from "@/components/common/CustomTabs";
+import { CustomCard } from "./CustomCard";
 const tabProps: NonEmptyArray<TabProp> = [
     {
         name: "01.HOME",
@@ -25,35 +26,42 @@ const tabSize = "size-25";
 
 export const Header = () => {
     return (
-        <header className="col-span-2 flex items-center border border-border divide-x divide-border bg-content-background text-input">
-            <Link
-                href="/"
-                aria-label="トップページへ戻る"
-                className="flex-1 flex p-4 h-full"
+        <header className="col-span-2 flex items-center justify-between border border-border bg-content-background text-input h-25 ">
+            <CustomCard
+                label="Top"
+                className="h-full w-auto p-0 m-0 border-transparent"
             >
-                <Image
-                    src={localImage}
-                    alt="プロフィール画像"
-                    height={64}
-                    width={64}
-                    className="mr-4 rounded-full object-cover"
-                />
-                <div className="text-border">
-                    <p>Personal Log</p>
-                    <div className="flex items-end gap-2">
-                        <h1 className="text-foreground text-3xl font-bold">
-                            DEV活
-                        </h1>
-                        <p>RyoK73@omarchy</p>
+                <Link
+                    href="/"
+                    aria-label="トップページへ戻る"
+                    className="flex h-full border border-transparent m-2"
+                >
+                    <Image
+                        src={localImage}
+                        alt="プロフィール画像"
+                        height={64}
+                        width={64}
+                        className="w-auto mr-4 rounded-full object-cover"
+                    />
+                    <div className="text-border flex flex-col justify-center text-[1rem]">
+                        <p>Personal Log</p>
+                        <div className="flex items-end gap-2">
+                            <h1 className="text-foreground text-3xl font-bold text-nowrap">
+                                DEV活
+                            </h1>
+                            <p>RyoK73@omarchy</p>
+                        </div>
                     </div>
+                </Link>
+            </CustomCard>
+            <div className="flex divide-x divide-border border-l">
+                <div className="flex h-full justify-end">
+                    <CustomTabs tabs={tabProps} className={tabSize} />
                 </div>
-            </Link>
-            <div className="flex h-full justify-end">
-                <CustomTabs tabs={tabProps} className={tabSize} />
+                <SwitchTheme
+                    className={`flex justify-center items-center ${tabSize}`}
+                />
             </div>
-            <SwitchTheme
-                className={`flex justify-center items-center ${tabSize}`}
-            />
         </header>
     );
 };
