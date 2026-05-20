6ceffd25786f110622f45cd056abe79a6fe040f0
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-20 15:08:44 +0900

    fix:コンポーネント外でusePathNameを使用したバグを修正
    - コンポーネント内にisActiveTab関数をCustomTabsコンポーネント内に移動
    - 多用していたtabLinkをhref,tabHrefに変更し可読性を向上

--- src/components/common/CustomTabs.tsx
+++ src/components/common/CustomTabs.tsx
@@ -6,14 +6,23 @@ import userTags from "@/user-tag.json";
 
 export const CustomTabs = ({ className }: { className?: string }) => {
     const pathName = usePathname();
-    const getLinkClass = (tabLink: string): string[] => {
+
+    const isActiveTab = (href: string): boolean => {
+        return (
+            href === pathName ||
+            (href !== "/" && pathName.startsWith(`/blog/tag/${href}`))
+        );
+    };
+
+    const getLinkClass = (href: string): string[] => {
         return [
             " bg-background flex justify-center items-center",
-            isActiveTab(tabLink)
+            isActiveTab(href)
                 ? "border border-vivid text-vivid"
                 : "bg-background/0 text-input",
         ];
     };
+
     return (
         <nav
             className={cn("flex h-auto justify-center divide-x divide-border")}
@@ -26,26 +35,18 @@ export const CustomTabs = ({ className }: { className?: string }) => {
                 {"01.HOME"}
             </Link>
             {Object.keys(userTags).map((tag, index) => {
-                const tabLink: string = `/blog/tag/${tag}`;
+                const tagHref: string = `/blog/tag/${tag}`;
                 const tabName: string = `${(index + 2).toString().padStart(2, "0")}.${tag.toUpperCase()}`;
                 return (
                     <Link
                         key={tag}
-                        href={tabLink}
-                        className={cn(getLinkClass(tag), className)}
+                        href={tagHref}
+                        className={cn(getLinkClass(tagHref), className)}
                     >
                         {tabName}
                     </Link>
                 );
             })}
         </nav>
-    );
-};
-
-const isActiveTab = (tabLink: string): boolean => {
-    const pathName = usePathname();
-    return (
-        tabLink === pathName ||
-        (tabLink !== "/" && pathName.startsWith(`/blog/tag/${tabLink}`))
     );
 };
