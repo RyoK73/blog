644b6ae620cfc4ce3d0943b28eb59bd820bc9238
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-22 10:53:27 +0900

    feat:タブの遷移・スタイルを変更
    - ルーティングが、"/","/blog/[tag]","/about"の場合、四角縁取り
    - ルーティングが、"/blog/[tag]/[slug]"の場合、"[tag]"のタブの下枠線だけ表示

--- src/components/common/CustomTabs.tsx
+++ src/components/common/CustomTabs.tsx
@@ -4,14 +4,38 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import userTags from "@/user-tag.json";
 
+type Tab = {
+    href: string;
+    name: string;
+    prefix: boolean;
+};
+
+const tabStyle = {
+    active: "border border-vivid text-vivid",
+    semiactive: "border-b border-b-vivid text-vivid",
+    inactive: "bg-background/0 text-input",
+};
+
 export const CustomTabs = ({ className }: { className?: string }) => {
     const pathName = usePathname();
 
     const isActiveTab = (href: string): boolean => {
         return href === pathName || (href !== "/" && pathName.startsWith(href));
     };
+    const getTabStyle = (tab: Tab): string => {
+        const tag = tab.name.toLowerCase();
+        if (pathName == tab.href) {
+            return tabStyle.active;
+        } else if (
+            tab.href !== "/" &&
+            pathName !== tab.href &&
+            pathName.startsWith(tab.href)
+        ) {
+            return tabStyle.semiactive;
+        } else return tabStyle.inactive;
+    };
 
-    const tabs = [
+    const tabs: Tab[] = [
         { href: "/", name: "HOME", prefix: true },
         ...Object.keys(userTags).map((tag) => ({
             href: `/blog/${tag}`,
@@ -30,10 +54,8 @@ export const CustomTabs = ({ className }: { className?: string }) => {
                         key={tab.href}
                         href={tab.href}
                         className={cn(
-                            " bg-background flex justify-center items-center",
-                            isActiveTab(tab.href)
-                                ? "border border-vivid text-vivid"
-                                : "bg-background/0 text-input",
+                            "flex justify-center items-center",
+                            getTabStyle(tab),
                             className,
                         )}
                     >
