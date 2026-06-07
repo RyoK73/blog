c07f32841a69420f3565ceafd65877f1c9dd6cfb
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-07 15:21:24 +0900

    fix:initialThemeでhydration errorを解決
    問題
    - 以前行ったuseStateでのhydration errorの修正を取り消し
    - これは、SSR時との差分表示時のUIのフラッシュ問題を抱えていた。また、<CustomButton>にonClickが指定されていないためUX上のバグも引き起こしていた。
    解決策
    - <ThemeProviders>のinitialThemeにdarkを指定した
    - これでSSR時とReactレンダリング時のthemeの差異によるUIフラッシュは解消。
    - 同時にonClickが空の<CustomButton>も削除

--- src/components/common/SwitchTheme.tsx
+++ src/components/common/SwitchTheme.tsx
@@ -1,33 +1,24 @@
 "use client";
 import { useTheme } from "@teispace/next-themes";
-import { useState, useEffect } from "react";
 import { LuSun } from "react-icons/lu";
 import { LuMoonStar } from "react-icons/lu";
 import { CustomButton } from "./CustomButton";
 
 export const SwitchTheme = ({ className }: { className?: string }) => {
   const { setTheme, theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  // レンダーされたときに
-  // eslint-disable-next-line react-hooks/set-state-in-effect
-  useEffect(() => setMounted(true), []);
 
-  if (!mounted) {
-    return (
-      <CustomButton className={className}>
-        <LuMoonStar />{" "}
-      </CustomButton>
-    );
-  } else {
-    return (
-      <CustomButton
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-        }}
-        className={className}
-      >
-        {theme == "dark" ? <LuSun /> : <LuMoonStar />}
-      </CustomButton>
-    );
-  }
+  return (
+    <CustomButton
+      onClick={() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+      className={className}
+    >
+      {theme === "dark" ? (
+        <LuSun className="hidden dark:block" />
+      ) : (
+        <LuMoonStar className="block dark:hidden" />
+      )}
+    </CustomButton>
+  );
 };
