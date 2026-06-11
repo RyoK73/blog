678dea7a0608e638acf051c35b13f263a679f9ba
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-11 15:17:22 +0900

    fix:categoryEntriesのカテゴリが重複して生成される問題を修正
    原因
    - PostData[]から生成していたため、同じカテゴリを持つ記事がある場合categoryEntriesの中でカテゴリが重複していた
    解決
    - user-category.jsonのkeyから生成することで、カテゴリを本来の数に絞ることができた

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from "next";
 import { getAllPostData } from "@/lib/post";
+import userCategory from "@/user-category.json";
+
+const categories = Object.keys(userCategory);
 
 export const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
@@ -13,8 +16,9 @@ export const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
     changeFrequency: "weekly" as const,
     priority: 0.8,
   }));
-  const categoryEntries = posts.map((post) => ({
-    url: `${baseUrl}/blog/${post.category}`,
+
+  const categoryEntries = categories.map((category) => ({
+    url: `${baseUrl}/blog/${category}`,
     changeFrequency: "weekly" as const,
     priority: 0.8,
   }));
