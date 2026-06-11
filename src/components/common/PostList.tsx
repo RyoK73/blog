4af75ca3b40fcf749bd8580a9a0b06baaa4d65ed
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-11 10:43:10 +0900

    feat:descriptionから簡易記事紹介を追加
    - それに伴いラベルの位置を移動

--- src/components/common/PostList.tsx
+++ src/components/common/PostList.tsx
@@ -5,35 +5,40 @@ import Link from "next/link";
 import path from "path";
 
 export const Posts = async ({ category }: { category?: string }) => {
-    const posts = await getAllPostData();
-    const sortedPosts = posts.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
-    const filteredPosts = category
-        ? sortedPosts.filter((sortedPost) => sortedPost.category === category)
-        : sortedPosts;
-    return (
-        <ul className="w-full flex flex-col gap-5">
-            {filteredPosts.map((post, index) => (
-                <li key={post.slug}>
-                    <CustomCard
-                        label={`P${(index + 1).toString().padStart(3, "0")}`}
-                        className="relative hover:border-foreground hover:transition-colors duration-500"
-                    >
-                        <time>{post.createdAt}</time>
-                        <Link
-                            href={path.join("/blog", post.category, post.slug)}
-                            className="after:absolute after:inset-0"
-                        >
-                            <h2 className="text-foreground font-bold text-lg">
-                                {post.title}
-                            </h2>
-                        </Link>
-                        <CustomCategory
-                            category={post.category}
-                            className="relative z-1 text-sm"
-                        />
-                    </CustomCard>
-                </li>
-            ))}
-        </ul>
-    );
+  const posts = await getAllPostData();
+  const sortedPosts = posts.sort((a, b) =>
+    b.createdAt.localeCompare(a.createdAt),
+  );
+  const filteredPosts = category
+    ? sortedPosts.filter((sortedPost) => sortedPost.category === category)
+    : sortedPosts;
+  return (
+    <ul className="w-full flex flex-col gap-5">
+      {filteredPosts.map((post, index) => (
+        <li key={post.slug}>
+          <CustomCard
+            label={`P${(index + 1).toString().padStart(3, "0")}`}
+            className="relative hover:border-foreground hover:transition-colors duration-500"
+          >
+            <div className="flex gap-5 items-center mb-1">
+              <CustomCategory
+                category={post.category}
+                className="relative z-1 text-sm"
+              />
+              <time>{post.createdAt}</time>
+            </div>
+            <Link
+              href={path.join("/blog", post.category, post.slug)}
+              className="after:absolute after:inset-0"
+            >
+              <h2 className="text-foreground font-bold text-lg">
+                {post.title}
+              </h2>
+            </Link>
+            <p>{post.description}</p>
+          </CustomCard>
+        </li>
+      ))}
+    </ul>
+  );
 };
